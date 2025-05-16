1451146f81526b3185e8bb24affb3df7c3166483
ishuvam <ishuvam549@gmail.com>
2025-05-16 23:31:37 +0530

    Commit

--- server/src/router/user.ts
+++ server/src/router/user.ts
@@ -2,5 +2,8 @@ import { Router,Response,Request } from "express";
 
 const userRouter = Router();
 
+userRouter.post('/chat',(req: Request, res: Response)=>{
+    res.send('Hello ');
+})
 
 export default userRouter;
