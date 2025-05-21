7424100bfe475612f7e0631740679dd24d254506
ishuvam <ishuvam549@gmail.com>
2025-05-21 23:58:26 +0530

    Commit

--- server/src/router/user.ts
+++ server/src/router/user.ts
@@ -6,7 +6,11 @@ userRouter.post('/chat',(req: Request, res: Response)=>{
     const query = req.body.query;
     const context = req.body.context;
     const systemPrompt = req.body.systemPrompt;
-    const hasPaid = req.body.hasPaid; 
+    const hasPaid = req.body.hasPaid;
+    
+    if(!query && !context && !systemPrompt && !hasPaid){
+        res.status(406).json({message:''})
+    }
 })
 
 export default userRouter;
