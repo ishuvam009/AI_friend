5509d8c02f0d571bb517b3337a3a1590e1d80389
ishuvam <ishuvam549@gmail.com>
2025-05-22 23:53:53 +0530

    Commit

--- server/src/router/user.ts
+++ server/src/router/user.ts
@@ -5,12 +5,14 @@ const userRouter = Router();
 userRouter.post('/chat',(req: Request, res: Response)=>{
     const query = req.body.query;
     const context = req.body.context;
-    const systemPrompt = req.body.systemPrompt;
+    const systemPrompt = req.body.systemPrompt; //should be stores not should asked every time.
     const hasPaid = req.body.hasPaid;
     
-    if(!query && !context && !systemPrompt && !hasPaid){
+    if(!query && !context && !hasPaid){
         res.status(406).json({message:''})
     }
+
+    //(query + context) + system prompt 
 })
 
 export default userRouter;
